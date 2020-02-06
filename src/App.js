76bf5624b32038a7126d20c45e4a4318afa8fd8c
AliAhmetov <a.ahmetovx20@gmail.com>
2020-02-06 13:56:52 +0600

    frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import MovieList from './components/movie-list';
+import MovieDetails from './components/movie-details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    movies: [],
+    selectedMovie: null
+  }
+
+  componentDidMount() {
+    //fetch data
+    fetch(' http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 3aca90cebc8d83a834cc97937af58ccf141f4492'
+      }
+    }).then( resp => resp.json())
+    .then( res => this.setState({movies: res}))
+    .catch( error => console.log(error))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Movie Rater</h1>
+        <div className='layout'>
+          <MovieList movies={this.state.movies}/>
+          <MovieDetails movie={this.state.movie}/>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/movie-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MovieList(props) {
+
+    return (
+        <div>
+        {
+            props.movies.map(movie => {
+                return <h3 key={movie.id}>{movie.title}</h3>
+            })
+        }
+        </div>
+    )
+    }
+
+export default MovieList;
