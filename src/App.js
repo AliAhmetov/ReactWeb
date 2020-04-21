632272081d7908109d63acc1a34cf04f2d30996b
AliAhmetov <a.ahmetovx20@gmail.com>
2020-04-21 20:53:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,33 +2,71 @@ import React, { Component } from 'react';
 import './App.css';
 import MovieList from './components/movie-list';
 import MovieDetails from './components/movie-details';
+import MovieForm from './components/movie-form';
+import { withCookies } from 'react-cookie';
+var FontAwesome = require('react-fontawesome');
 
 class App extends Component {
   
   state = {
     movies: [],
-    selectedMovie: null
+    selectedMovie: null,
+    editedMovie: null,
+    token: this.props.cookies.get('mr-token')
   }
 
   componentDidMount() {
-    //fetch data
+    if(this.state.token) {
+      //fetch data
     fetch(' http://127.0.0.1:8000/api/movies/', {
       method: 'GET',
       headers: {
-        'Authorization': 'Token 3aca90cebc8d83a834cc97937af58ccf141f4492'
+        'Authorization': `Token ${this.state.token}`
       }
     }).then( resp => resp.json())
     .then( res => this.setState({movies: res}))
     .catch( error => console.log(error))
+    } else {
+      window.location.href ='/';
+    }
+  }
+
+  loadMovie = movie => {
+    this.setState({selectedMovie: movie, editedMovie: null});
+  }
+  movieDeleted = selMovie => {
+    const movies = this.state.movies.filter( movie => movie.id !== selMovie.id );
+    this.setState({movies: movies, selectedMovie: null})
+  }
+  editClicked = selMovie => {
+    this.setState({editedMovie: selMovie});
+  }
+  newMovie = () => {
+    this.setState({editedMovie: {title: '', description: ''}});
+  }
+  cancelForm = () => {
+    this.setState({editedMovie: null});
+  }
+  addMovie = movie => {
+    this.setState({movies: [...this.state.movies, movie]});
   }
 
   render () {
     return (
       <div className="App">
-        <h1>Movie Rater</h1>
+        <h1>
+          <FontAwesome name="film"/>
+          <span>Movie Rater</span>
+        </h1>
         <div className='layout'>
-          <MovieList movies={this.state.movies}/>
-          <MovieDetails movie={this.state.movie}/>
+          <MovieList movies={this.state.movies} movieClicked={this.loadMovie} token={this.state.token}
+            movieDeleted={this.movieDeleted} editClicked={this.editClicked} newMovie={this.newMovie}/>
+          <div>
+            { !this.state.editedMovie ? 
+            <MovieDetails movie={this.state.selectedMovie} updateMovie={this.loadMovie} token={this.state.token}/>
+            : <MovieForm movie={this.state.editedMovie} cancelForm={this.cancelForm}
+            newMovie={this.addMovie} editedMovie={this.loadMovie} token={this.state.token}/>}
+          </div>
         </div>
       </div>
     );
@@ -36,4 +74,4 @@ class App extends Component {
   
 }
 
-export default App;
+export default withCookies(App);
--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,12 +1,71 @@
 import React, { Component } from "react";
+var FontAwesome = require('react-fontawesome');
 
 class MovieDetails extends Component {
 
+    state = {
+        highlighted: -1
+    }
+
+    highlitghRate = high => evt => {
+        this.setState({highlighted: high});
+    }
+
+    rateClicked = stars => evt => {
+        //fetch data
+        fetch(`${process.env.REACT_APP_API_URL}/api/movies/${this.props.movie.id}/rate_movie/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${this.props.token}`
+            },
+            body: JSON.stringify({stars: stars +1 })
+        }).then( resp => resp.json())
+        .then( res => this.getDetails())
+        .catch( error => console.log(error))
+    }
+
+    getDetails = () => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/movies/${this.props.movie.id}/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${this.props.token}`
+            },
+        }).then( resp => resp.json())
+        .then( res => this.props.updateMovie(res))
+        .catch( error => console.log(error))
+    }
+    
+
     render () {
+
+        const mov = this.props.movie;
+
         return (
         <React.Fragment>
-            { this.props.movie ? (
-                <h3>{this.props.movie.title}</h3>
+            { mov ? (
+                <div>
+                    <h3>{mov.title}</h3>
+                    <FontAwesome name="star" className={mov.avg_rating > 0 ? 'orange' : ''}/>
+                    <FontAwesome name="star" className={mov.avg_rating > 1 ? 'orange' : ''}/>
+                    <FontAwesome name="star" className={mov.avg_rating > 2 ? 'orange' : ''}/>
+                    <FontAwesome name="star" className={mov.avg_rating > 3 ? 'orange' : ''}/>
+                    <FontAwesome name="star" className={mov.avg_rating > 4 ? 'orange' : ''}/>
+                    ({mov.no_of_ratings})
+                    <p>{mov.description}</p>
+
+                    <div className='rate-container'>
+                        <h2>Rate it !!!</h2>
+                        {
+                            [...Array(5)].map( (e, i) => {
+                                return <FontAwesome key={i} name="star" className={this.state.highlighted > i - 1 ? 'purple' : ''}
+                                    onMouseEnter={this.highlitghRate(i)} onMouseLeave={this.highlitghRate(-1)} onClick={this.rateClicked(i)}/>
+                            })
+                        }
+
+                    </div>
+                </div>
             ) : null }
         </React.Fragment>
         )
